26ce5d5d975d28131488061822778c877d901f86
NathanBENAIS <96385008+NathanBENAIS@users.noreply.github.com>
2024-05-22 22:13:28 +0200

    exo 3

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@ cd worker/
 docker build -t nathanbenais/worker .
 docker push nathanbenais/worker
 kubectl apply -f pod.yaml
-kubectl apply -f deployment.yaml
+# kubectl apply -f deployment.yaml
+kubectl apply -f worker-mult-deployment.yaml
+kubectl apply -f worker-add-deployment.yaml
 kubectl apply -f worker-service.yaml
 cd ..
 cd planner/
 docker build -t nathanbenais/planner .
 docker push nathanbenais/planner
 kubectl apply -f pod.yaml
-kubectl apply -f deployment.yaml
 kubectl apply -f planner-service.yaml
--- delete.sh
+++ delete.sh
@@ -3,11 +3,12 @@ docker rmi nathanbenais/planner
 
 cd worker/
 kubectl delete -f pod.yaml
-kubectl delete -f deployment.yaml
+# kubectl delete -f deployment.yaml
+kubectl delete -f worker-add-deployment.yaml
+kubectl delete -f worker-mult-deployment.yaml
 kubectl delete -f worker-service.yaml
 
 cd ..
 cd planner/
 kubectl delete -f pod.yaml
-kubectl delete -f deployment.yaml
 kubectl delete -f planner-service.yaml
